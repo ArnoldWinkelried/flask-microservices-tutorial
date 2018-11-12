87a841e6c317dba75dd6721b5ea14f809fa75e02
ArnoldWinkelried <winkel.pawel@gmail.com>
2018-11-12 18:23:12 +0100

    File structure clean up; switched Flask version

---
+++ services/users/project/config.py
@@ -0,0 +1,20 @@
+# project/config.py
+
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    pass
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    pass
